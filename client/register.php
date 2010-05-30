9ad695f4b3fd3bbeb15729d9a82fff3bc5dbaee2
Alexandre Passant <alex@passant.org>
2010-05-30 15:00:09 +0000

    Ability to unregister

--- client/register.php
+++ client/register.php
@@ -1,13 +1,31 @@
 <?php
 
 require_once("./subscriber.php");
+
 require_once("./config.php");
 require_once('./simplepie/simplepie.inc');
 
 parse_str($_SERVER['QUERY_STRING']);
 
-// If there's a query, let's subscribe
-if($query) {
+function curl_get($url) {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$response = curl_exec($ch);
+		if ($error = curl_error($ch)) {
+			return array("$error.", "", 0);
+		}
+
+		$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		$status_line = substr($response, 0, strcspn($response, "\n\r"));
+		curl_close($ch);
+		print_r($response);
+		return array($response, $status_line, $status_code);
+}
+
+// Registration
+if($action=='r' && $query) {
 	// Get the feed
 	$url = $endpoint.'?query='.urlencode($query);
 	$data = file_get_contents($url);
@@ -25,11 +43,23 @@ if($query) {
 	if($s) {
 		echo "Successfully registered $url to ${hub}subscribe with callback ".BASENAME."/endpoint.php";
 	}
-
+}
+// Un-registration
+elseif($action=='u') {
+	$s = new Subscriber($hub.'subscribe', BASENAME."/endpoint.php");
+	$s->unsubscribe($feed);
+	if($s) {
+		echo "Successfully unregistered $feed from ${hub}";
+	}
+}
 // Display the form
-} else {
+else {
 	echo "
+<h2>Register</h2>
+
 <form action='register.php'>
+<input type='hidden' name='action' value='r'/>
+
 Query:
 
 <textarea name=\"query\" rows=\"10\" cols=\"80\">
@@ -52,6 +82,30 @@ sparqlPuSH interface:
 
 <input type=\"submit\" value=\"Register\"/>
 </form>
+
+<hr/>
+
+<h2>Un-register</h2>
+
+<h2>Register</h2>
+
+<form action='register.php'>
+<input type='hidden' name='action' value='u'/>
+
+Feed URL:
+<input name=\"feed\" size=\"80\"/>
+
+
+<br/>
+
+Hub:
+<input name=\"hub\" size=\"80\" value=\"http://pubsubhubbub.appspot.com/\"/>
+
+<br/>
+
+<input type=\"submit\" value=\"Un-register\"/>
+</form>
+
 ";
 
 }
